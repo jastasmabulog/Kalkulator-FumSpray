a1a1048eb2889dd5306ccd5aae8664050f7cae89
jastasmabulog <jastasma.bulog@gmail.com>
2026-04-17 14:16:52 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://kalkulator-fum-spray.vercel.app";
 
 // ================= LOAD KANWIL =================
 async function loadKanwil() {
